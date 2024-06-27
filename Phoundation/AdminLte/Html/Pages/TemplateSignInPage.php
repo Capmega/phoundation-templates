2f5cacf377bc18d7349ff838f94cbd54968c1766
Sven Olaf Oostenbrink <sven@medinet.ca>
2024-06-27 16:23:14 -0700

    Cleaned code
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/AdminLte/Html/Pages/TemplateSignInPage.php
+++ Phoundation/AdminLte/Html/Pages/TemplateSignInPage.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Class TemplateSignInPage
+ *
+ *
+ *
+ * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2024 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Phoundation\Web
+ */
+
 declare(strict_types=1);
 
 namespace Templates\Phoundation\AdminLte\Html\Pages;
@@ -12,17 +23,6 @@ use Phoundation\Web\Html\Template\TemplateRenderer;
 use Phoundation\Web\Http\UrlBuilder;
 use Phoundation\Web\Requests\Response;
 
-
-/**
- * Class TemplateSignInPage
- *
- *
- *
- * @author Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
- * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
- * @copyright Copyright (c) 2024 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
- * @package Phoundation\Web
- */
 class TemplateSignInPage extends TemplateRenderer
 {
     public function render(): ?string
